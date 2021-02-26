ad8080f24f19d9947ef1a9898659bb504ca2d0ff
wen-code <707613482@qq.com>
2021-02-26 13:47:58 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <el-button type="primary" size="default" >111</el-button>
+    
+  </div>
+</template>
+<script>
+export default {
+  name: 'App'
+}
+</script>
+<style scoped>
+  
+</style>
